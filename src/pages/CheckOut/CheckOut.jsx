2802071f85dc5e7b03fa51c8fb7f7ad6826f3f69
shahinexy <shahinexy@gmail.com>
2024-05-03 23:45:34 +0600

    send data in databse by form

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -1,10 +1,85 @@
+import { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { authContex } from "../../AuthProvider/AuthProvider";
+import { useForm } from "react-hook-form";
 
 const CheckOut = () => {
-    return (
-        <div>
-            this is check out
-        </div>
-    );
+  const loaderData = useLoaderData();
+  const {user} = useContext(authContex)
+  const {
+    register,
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = data =>{
+    const datas = {...data, title: loaderData.title};
+    fetch('http://localhost:5000/checkOut', {
+        method: 'POST',
+        headers: {
+            'content-type' : 'application/json'
+        },
+        body: JSON.stringify(datas)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+  return (
+    <div>
+      <div className="flex flex-col gap-5 items-center justify-center my-5">
+        <h2 className="text-3xl font-bold">Check out: {loaderData.title}</h2>
+        <img className="w-72" src={loaderData.img} alt="" />
+      </div>
+
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex gap-5">
+            <label className="label">
+              <span className="label-text">Name:</span>
+            </label>
+            <input
+              {...register("name")}
+              type="text"
+              placeholder="name"
+              className="input input-bordered"
+              defaultValue={user?.displayName}
+            />
+            <label className="label">
+              <span className="label-text">Date</span>
+            </label>
+            <input
+              {...register("date")}
+              type="date"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="flex gap-5">
+            <label className="label">
+              <span className="label-text">Email:</span>
+            </label>
+            <input
+              {...register("email")}
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              defaultValue={user?.email}
+            />
+            <label className="label">
+              <span className="label-text">Price</span>
+            </label>
+            <input
+              {...register("price")}
+              type="text"
+              className="input input-bordered"
+              defaultValue={'$'+ loaderData.price}
+            />
+          </div>
+          <button className="btn">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CheckOut;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,3 +1,4 @@
+import CheckOut from "../pages/CheckOut/CheckOut";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Login/Register";
@@ -10,23 +11,28 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root></Root>,
     children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
-        {
-            path: '/orderReview',
-            element: <OrderReview></OrderReview>
-        },
-        {
-            path: '/login',
-            element: <Login></Login>
-        },
-        {
-            path: '/register',
-            element: <Register></Register>
-        }
-    ]
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/orderReview",
+        element: <OrderReview></OrderReview>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/checkOut/:id",
+        element: <CheckOut></CheckOut>,
+        loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
+      },
+    ],
   },
 ]);
 
