b8f5146d52a99ba8ff1e0cfdcac07e17405ad44c
shahinexy <shahinexy@gmail.com>
2024-05-04 09:16:24 +0600

    book confirm function add

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -14,20 +14,39 @@ const Booking = () => {
       });
   }, [user]);
 
-  const handleDelete = id =>{
+  const handleDelete = (id) => {
     console.log(id);
     fetch(`http://localhost:5000/checkOut/${id}`, {
-      method: "DELETE"
+      method: "DELETE",
     })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      if(data.deletedCount >0){
-        const remaining = booking.filter(data => data._id !== id);
-        setBooking(remaining)
-      }
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          const remaining = booking.filter((data) => data._id !== id);
+          setBooking(remaining);
+        }
+      });
+  };
+
+  const handleConfirm = (id) => {
+    fetch(`http://localhost:5000/checkOut/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "confirm" }),
     })
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const remaining = booking.filter((data) => data._id !== id);
+        const updated = booking.find((data) => data._id === id);
+        updated.status = "confirm";
+        const newData = [updated, ...remaining];
+        setBooking(newData);
+      });
+  };
 
   return (
     <div>
@@ -42,16 +61,36 @@ const Booking = () => {
                 <th>Name</th>
                 <th>Job</th>
                 <th>Favorite Color</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
               {/* row 1 */}
               {booking.map((data) => (
                 <tr key={data._id}>
-                  <th><button onClick={() => handleDelete(data._id)} className="btn btn-sm btn-circle bg-black text-white font-semibold">X</button></th>
+                  <th>
+                    <button
+                      onClick={() => handleDelete(data._id)}
+                      className="btn btn-sm btn-circle bg-black text-white font-semibold"
+                    >
+                      X
+                    </button>
+                  </th>
                   <td>{data.name}</td>
                   <td>{data.title}</td>
                   <td>{data.price}</td>
+                  <td>
+                    {data?.status === "confirm" ? (
+                      <span className="text-blue-500 font-bold">Confirmed</span>
+                    ) : (
+                      <button
+                        onClick={() => handleConfirm(data._id)}
+                        className="btn"
+                      >
+                        Pleace Confirm
+                      </button>
+                    )}
+                  </td>
                 </tr>
               ))}
             </tbody>
